f7e32a5dca1a148f24b31c24e152e0232dd6fe67
Tulio-Rangel <tjrn20@hotmail.com>
2025-02-13 11:59:42 -0500

    feat: jwt validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("org.slf4j:slf4j-api")
     runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
     compileOnly("org.projectlombok:lombok")
